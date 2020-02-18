e425bc76a6231c2cce74daac45f8e7d9dde8607a
Ernie Hsiung <ernie@whereby.us>
2020-02-18 13:44:43 -0500

    my first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-class App extends React.Component {
-  state = { serverMessage: '' };
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-  componentDidMount() {
-    fetch('/api/demo')
-      .then(response => response.json())
-      .then(data => this.setState({ serverMessage: data.message }));
-  }
+  useEffect(() => {
+    const callOurYelpAPI = async () => {
+      const resp = await axios.get('/api/yelp');
+      setPlaces(resp.data);
+      setLoading(false);
+    };
+    callOurYelpAPI();
+  }, []);
 
-  render() {
-    return (
-      <div id="demo">
-        <h1>Hello from client/src/App.js</h1>
-        <h1>{this.state.serverMessage}</h1>
-      </div>
-    );
-  }
-}
+  return (
+    <div id="app">
+      <h1>WYNCODE DOES LUNCH</h1>
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <ul>
+          {places.map(place => (
+            <li key={place.id}>{place.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- server.js
+++ server.js
@@ -4,24 +4,43 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
+
   // Handle React routing, return all requests to React app
   app.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-// JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-app.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
+const getYelpAPI = async () => {
+  return axios.get(
+    'https://api.yelp.com/v3/businesses/search?location=2650 NW 5th Ave Miami FL',
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`
+      }
+    }
+  );
+};
+
+/**
+ * Calls our proxied API, which helps us bypass
+ * CORS issues and let's us hide API keys.
+ */
+app.get('/api/yelp', async (request, response) => {
+  try {
+    const resp = await getYelpAPI();
+    response.json(resp.data.businesses);
+  } catch (e) {
+    console.log(e);
+    response.status(500).send({ error: e.message });
+  }
 });
-// END DEMO
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
