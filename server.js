c2182ad27c017def8f68c9224c8fdaee7edd219c
Ernie Hsiung <ernie@whereby.us>
2020-02-18 14:21:20 -0500

    fix bug for APIs redirecting

--- server.js
+++ server.js
@@ -7,16 +7,6 @@ const path = require('path');
 const axios = require('axios');
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 const getYelpAPI = async () => {
   return axios.get(
     'https://api.yelp.com/v3/businesses/search?location=2650 NW 5th Ave Miami FL',
@@ -41,6 +31,16 @@ app.get('/api/yelp', async (request, response) => {
     response.status(500).send({ error: e.message });
   }
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
